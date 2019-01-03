16d73c652cf4fedf3bfeb24f03b0b62d3713a566
Emanuel Barrera <emanuel@eventbrite.com>
2019-01-03 00:32:30 -0300

    Update Composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitbee3847ac540e2669a299766cb904f7b
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitbee3847ac540e2669a299766cb904f7b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitbee3847ac540e2669a299766cb904f7b::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
