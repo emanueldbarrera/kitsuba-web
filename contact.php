72cf0a022a653cf7af2a8c087bdb77459ac0b358
Emanuel Barrera <emanuel@eventbrite.com>
2019-01-07 21:46:07 -0300

    debug

--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
 <?php
 
 // Debug lines
-// ini_set('display_startup_errors', 1);
-// ini_set('display_errors', 1);
-// error_reporting(-1);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 
 require_once "vendor/autoload.php";
 
